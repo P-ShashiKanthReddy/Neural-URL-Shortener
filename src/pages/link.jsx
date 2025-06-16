5b8bfd4552853baead52e7f7f0c2a3dc3f15204f
P Shashi Kanth Reddy <shashikanth8938@gmail.com>
2025-06-16 18:32:06 +0530

    Replace neural.in with neurals-five.vercel.app

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -76,11 +76,11 @@ const LinkPage = () => {
             {url?.title}
           </span>
           <a
-            href={`https://neural.in/${link}`}
+            href={`https://neurals-five.vercel.app/${link}`}
             target="_blank"
             className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
           >
-            https://neural.in/{link}
+            https://neurals-five.vercel.app/{link}
           </a>
           <a
             href={url?.original_url}
@@ -97,7 +97,7 @@ const LinkPage = () => {
             <Button
               variant="ghost"
               onClick={() =>
-                navigator.clipboard.writeText(`https://neural.in/${link}`)
+                navigator.clipboard.writeText(`https://neurals-five.vercel.app/${link}`)
               }
             >
               <Copy />
