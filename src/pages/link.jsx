689306342a8917d3c70c681f87fc1e12953fa29b
P Shashi Kanth Reddy <shashikanth8938@gmail.com>
2025-06-22 17:59:13 +0530

    Updated link.jsx

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -76,11 +76,11 @@ const LinkPage = () => {
             {url?.title}
           </span>
           <a
-            href={`https://imaginative-valkyrie-c0b150.netlify.app/${link}`}
+            href={`${window.location.origin}/${link}`}
             target="_blank"
             className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
           >
-            https://imaginative-valkyrie-c0b150.netlify.app/{link}
+            {window.location.origin}/{link}
           </a>
           <a
             href={url?.original_url}
@@ -97,7 +97,7 @@ const LinkPage = () => {
             <Button
               variant="ghost"
               onClick={() =>
-                navigator.clipboard.writeText(`https://imaginative-valkyrie-c0b150.netlify.app/${link}`)
+                navigator.clipboard.writeText(`${window.location.origin}/${link}`)
               }
             >
               <Copy />
